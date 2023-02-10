071842c360b3388aff54d24d6a9b79bdf96c8800
Tatyana <Tatyanakayan@mail.ru>
2023-02-10 22:45:37 +0300

    массив из трех и менее символов

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+//длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] array = new string[] { "Russia", "Krasnodar", "Chelyabinsk", "Ufa", ";o)"};
+void FillArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+void Diffarray(string[] arr)
+{
+       for (int i = 0; i < arr.Length; i++)
+    {
+        string element = arr[i];
+        if (element.Length <= 3)
+        {
+            arr[i] = element;
+        }
+        else arr[i] =  string.Empty;
+
+    }
+}
+FillArray(array);
+
+Diffarray(array);
+
+Console.Write("Массив из 3 и менее символов:");
+
+FillArray(array);
